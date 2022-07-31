d47331a2fcdb6ad8c44511c02008ca88f8613774
Майя Калицева <>
2022-07-31 16:33:14 +0300

    Initial commit

---
+++ Teleportation/Teleportation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teleportation
+//
+//  Created by Майя Калицева on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Teleportation/TeleportationUITests/TeleportationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TeleportationUITestsLaunchTests.swift
+//  TeleportationUITests
+//
+//  Created by Майя Калицева on 31.07.2022.
+//
+
+import XCTest
+
+class TeleportationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
